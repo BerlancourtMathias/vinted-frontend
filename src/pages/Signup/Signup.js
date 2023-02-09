542d18261eb063f2e845694e017ad232a45f7c8b
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-09 17:37:00 +0100

    Signup WIP, issue on the submit

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,32 +1,55 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Spinner from "../../components/Spinner/index";
 import axios from "axios";
 import "./signup.css";
 const Signup = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(true);
+  const [signup, setSignUp] = useState({
+    email: "",
+    username: "",
+    password: "",
+    newletter: false,
+  });
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    const fetchData = async () => {
+    const postSignUp = async () => {
       try {
-        const response = await axios.get(
-          "https://lereacteur-vinted-api.herokuapp.com/user/signup"
+        const response = await axios.post(
+          "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+          {
+            email: signup.email,
+            username: signup.username,
+            password: signup.password,
+            newsletter: signup.newsletter,
+          }
         );
+        setSignUp();
       } catch (error) {
         console.log(error.message);
       }
     };
-    fetchData();
+    postSignUp();
   }, []);
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div>
       Inscription coucou
       <form label="signUpForm">
-        <input type="text" placeholder="Nom d'utilisateur" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Mot de passe" />
-        <input type="checkbox" id="newLetter" />
+        <input
+          type="text"
+          placeholder="Nom d'utilisateur"
+          value={signup.username}
+        />
+        <input type="email" placeholder="Email" value={signup.email} />
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={signup.password}
+        />
+        <input type="checkbox" id="newLetter" value={signup.newletter} />
         <label for="newsLetter">S'inscrire à notre newsletter</label>
 
         <p id="CGU">
@@ -41,7 +64,15 @@ const Signup = () => {
           responsabilités en tant qu'utilisateur de Vinted, ainsi que la manière
           dont nous protégeons vos données personnelles.
         </p>
-        <input type="submit" value="S'inscrire" />
+        <input
+          type="submit"
+          value="S'inscrire"
+          accesskey="
+          "
+          onSubmit={() => {
+            postSignUp;
+          }}
+        />
         <span onClick={() => navigate("/login")}>
           Tu as déjà un compte ? connecte-toi!
         </span>
