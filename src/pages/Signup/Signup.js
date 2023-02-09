0feb1923b109f62e8a5090704ac425bdabdc9728
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-09 19:47:04 +0100

    signup finished without css

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,61 +1,84 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Spinner from "../../components/Spinner/index";
+import Cookies from "js-cookie";
 import axios from "axios";
 import "./signup.css";
 const Signup = () => {
   const navigate = useNavigate();
-  const [signup, setSignUp] = useState({
-    email: "",
-    username: "",
-    password: "",
-    newletter: false,
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    const postSignUp = async () => {
-      try {
-        const response = await axios.post(
-          "https://lereacteur-vinted-api.herokuapp.com/user/signup",
-          {
-            email: signup.email,
-            username: signup.username,
-            password: signup.password,
-            newsletter: signup.newsletter,
-          }
-        );
-        setSignUp();
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    postSignUp();
-  }, []);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newsletter, setNewsletter] = useState(false);
 
-  return isLoading ? (
-    <Spinner />
-  ) : (
+  const handleUsernameChange = (event) => {
+    const value = event.target.value;
+    setUsername(value);
+  };
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+  const handleEmailchange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+  const handleNewsletter = () => {
+    setNewsletter(!newsletter);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup",
+        {
+          email: email,
+          username: username,
+          password: password,
+          newsletter: newsletter,
+        }
+      );
+      console.log("response axios: ", response);
+      const token = response.data.token;
+      console.log("TOKEN :", token);
+      Cookies.set("token", token, { expires: 60 });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
     <div>
-      Inscription coucou
-      <form label="signUpForm">
+      <form label="signUpForm" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Nom d'utilisateur"
-          value={signup.username}
+          value={username}
+          onChange={handleUsernameChange}
         />
-        <input type="email" placeholder="Email" value={signup.email} />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailchange}
+        />
         <input
           type="password"
           placeholder="Mot de passe"
-          value={signup.password}
+          value={password}
+          onChange={handlePasswordChange}
         />
-        <input type="checkbox" id="newLetter" value={signup.newletter} />
-        <label for="newsLetter">S'inscrire à notre newsletter</label>
+        <input
+          type="checkbox"
+          id="newLetter"
+          value={newsletter}
+          onChange={handleNewsletter}
+        />
+        <label htmlFor="newsLetter">S'inscrire à notre newsletter</label>
 
         <p id="CGU">
           En vous inscrivant sur Vinted, vous attestez être majeur et
-          reconnaissez avoir pris connaissance et acceptez les{" "}
-          <span>conditions générales d'utilisation</span> ainsi que notre{" "}
+          reconnaissez avoir pris connaissance et acceptez les
+          <span>conditions générales d'utilisation</span> ainsi que notre
           <span>politique de confidentialité des données</span> de notre
           plateforme. Ces conditions régissent l'utilisation de nos services et
           sont importantes pour garantir une expérience utilisateur sûre et
@@ -67,11 +90,8 @@ const Signup = () => {
         <input
           type="submit"
           value="S'inscrire"
-          accesskey="
+          accessKey="
           "
-          onSubmit={() => {
-            postSignUp;
-          }}
         />
         <span onClick={() => navigate("/login")}>
           Tu as déjà un compte ? connecte-toi!
